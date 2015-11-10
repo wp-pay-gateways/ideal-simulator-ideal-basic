2ee7d8831f710b3b0a77af39455711e2992f904e
Remco Tolsma <info@remcotolsma.nl>
2015-11-10 12:35:58 +0100

    composer install

--- src/GatewayIntegration.php
+++ src/GatewayIntegration.php
@@ -4,4 +4,16 @@ class Pronamic_WP_Pay_Gateways_IDealSimulator_IDealBasic_GatewayIntegration {
 	public function __construct() {
 		$this->id = 'ideal-simulator-ideal-basic';
 	}
+
+	public function get_config_factory_class() {
+		return 'Pronamic_WP_Pay_Gateways_IDealBasic_ConfigFactory';
+	}
+
+	public function get_config_class() {
+		return 'Pronamic_WP_Pay_Gateways_IDealBasic_Config';
+	}
+
+	public function get_gateway_class() {
+		return 'Pronamic_WP_Pay_Gateways_IDealBasic_Gateway';
+	}
 }
