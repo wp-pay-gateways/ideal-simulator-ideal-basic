c7fc8d72c292511d0474c00f745a288f9c58b7a7
Remco Tolsma <info@remcotolsma.nl>
2015-11-20 15:58:50 +0100

    Make sure to call parent construct.

--- src/Integration.php
+++ src/Integration.php
@@ -10,6 +10,8 @@
  */
 class Pronamic_WP_Pay_Gateways_IDealSimulator_IDealBasic_Integration extends Pronamic_WP_Pay_Gateways_IDealBasic_AbstractIntegration {
 	public function __construct() {
+		parent::__construct();
+
 		$this->id       = 'ideal-simulator-ideal-basic';
 		$this->name     = 'iDEAL Simulator - iDEAL Lite / Basic';
 		$this->provider = 'ideal-simulator';
