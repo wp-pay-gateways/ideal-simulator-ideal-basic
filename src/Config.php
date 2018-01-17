7ed9e8d98f09f35b65c150999abc81233417b86e
Reüel van der Steege <reuel@pronamic.nl>
2018-01-17 12:01:48 +0100

    Happy 2018.

--- src/Config.php
+++ src/Config.php
@@ -3,7 +3,7 @@
 /**
  * Title: iDEAL Simulator - iDEAL Lite / Basic config
  * Description:
- * Copyright: Copyright (c) 2005 - 2016
+ * Copyright: Copyright (c) 2005 - 2018
  * Company: Pronamic
  *
  * @author Remco Tolsma
--- src/Integration.php
+++ src/Integration.php
@@ -3,7 +3,7 @@
 /**
  * Title: iDEAL Simulator - iDEAL Lite / Basic integration
  * Description:
- * Copyright: Copyright (c) 2005 - 2016
+ * Copyright: Copyright (c) 2005 - 2018
  * Company: Pronamic
  *
  * @author Remco Tolsma
